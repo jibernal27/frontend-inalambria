7d4af1b134266522fd7adc598e484c315a867110
Jairo Bernal <jairo.bernal@send.sd>
2020-02-12 13:50:41 -0500

    add doc folder

---
+++ docs/precache-manifest.8f0e189394c31ea9494ffaee9f036ec3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d57de9e4b7810ef59b",
+    "url": "/css/app.934fa2db.css"
+  },
+  {
+    "revision": "2a4a847447490f899eed",
+    "url": "/css/chunk-vendors.fbe9bade.css"
+  },
+  {
+    "revision": "dd372db5fbf1a2d9a6a8de7b85dd952d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69d57de9e4b7810ef59b",
+    "url": "/js/app.b35f761f.js"
+  },
+  {
+    "revision": "2a4a847447490f899eed",
+    "url": "/js/chunk-vendors.ded35685.js"
+  },
+  {
+    "revision": "2b5bc491a0afb3c6e6d124cfcc417227",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
